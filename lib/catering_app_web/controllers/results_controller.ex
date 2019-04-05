7ec69996c2707548089cee585fe4a81658203068
Jacob Piersall <jacobpiersall@gmail.com>
2019-04-05 18:36:39 +0000

    API requests added (partially) printed to elixir console

--- lib/catering_app_web/controllers/results_controller.ex
+++ lib/catering_app_web/controllers/results_controller.ex
@@ -5,6 +5,9 @@ defmodule CateringAppWeb.ResultsController do
   alias CateringApp.Menus.Menu
 
   def index(conn, %{"prefs" => prefs}) do
+    http_prefs = String.replace(prefs, " ", "%20")
+    results = HTTPoison.get("https://www.food2fork.com/api/search?key=d6203ad14cc7132eb562d765899b7d1d&q=" <> http_prefs)
+    IO.inspect(results)
     render(conn, "index.html", prefs: prefs)
   end
 end
