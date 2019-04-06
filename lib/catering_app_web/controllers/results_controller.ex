4d86e5dd3cebd15fe16675de6aa81d77c9899435
Jacob Piersall <jacobpiersall@gmail.com>
2019-04-06 20:23:45 +0000

    fix to bad food2fork link, shrunk images, hid broken images

--- lib/catering_app_web/controllers/results_controller.ex
+++ lib/catering_app_web/controllers/results_controller.ex
@@ -3,7 +3,8 @@ defmodule CateringAppWeb.ResultsController do
   def index(conn, %{"prefs" => prefs}) do
     IO.inspect("API Request sent!")
     http_prefs = String.replace(prefs, " ", "%20")
-    results = HTTPoison.get("https://www.food2fork.com/api/search?key=d6203ad14cc7132eb562d765899b7d1d&q=" <> http_prefs)
+    api_key = "3c67ca2f9ed8271d62d8759bb3356721"
+    results = HTTPoison.get("https://www.food2fork.com/api/search?key=" <> api_key <> "&q=" <> http_prefs)
     decResults = Poison.decode(elem(results, 1).body)
     listResults = decResults |> elem(1) |> Map.fetch("recipes") |> elem(1)
     render(conn, "index.html", prefs: prefs, results: listResults)
